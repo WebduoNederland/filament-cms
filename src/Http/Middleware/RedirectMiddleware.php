55924eb438eb0ed8855dcc027a221e6c4f0926ce
Finn Paes <finn@webduo.nl>
2024-12-21 16:32:43 +0100

    Change return type of redirect middleware

--- src/Http/Middleware/RedirectMiddleware.php
+++ src/Http/Middleware/RedirectMiddleware.php
@@ -3,14 +3,12 @@
 namespace WebduoNederland\FilamentCms\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use WebduoNederland\FilamentCms\Models\FilamentCmsRedirect;
 
 class RedirectMiddleware
 {
-    public function handle(Request $request, Closure $next): Response|RedirectResponse|Closure
+    public function handle(Request $request, Closure $next): mixed
     {
         $path = $request->path();
 
